5a2e2747eb49196550f246b19113e33dedeae019
CoyoteCodesAlot <71836050+CoyoteCodesAlot@users.noreply.github.com>
2021-04-05 10:50:04 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,15 @@ tl.fromTo('.nav-items > li', { opacity: 0, y: '-25%' }, { y: '0%', opacity: 1, d
 tl.fromTo('.navbar > h1', { opacity: 0, x: '25%' }, { x: '0%', opacity: 1, duration: 0.5, delay: 0.5 }, '-=1')
 tl.fromTo('.btn-primary', { opacity: 0, x: '-50%' }, { x: '0%', opacity: 1, duration: 0.5, delay: 1 }, '-=1')
 
+let open = false
+
 $(document).ready(() => {
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0)
+
+    $("div.github").click((e) => {
+        location.href = `https://github.com/coyotecodesalot/${$(e.target).attr("repository")}`
+    })
+
     $(".scrollTo").click((e) => {
         $([document.documentElement, document.body]).animate({
             scrollTop: $($(e.target).attr("to")).offset().top
@@ -16,5 +23,30 @@ $(document).ready(() => {
         $([document.documentElement, document.body]).animate({
             scrollTop: $($(e.target).attr("to")).offset().top
         }, 600);
+    })
+
+    $(".nav-links > li").click(e => {
+        $(".nav-mobile").removeClass("open")
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $($(e.target).attr("to")).offset().top
+        }, 600);
+        open = false
+    })
+
+    $(".bars").click(() => {
+        if (open) {
+            $(".nav-mobile").removeClass("open")
+            $(".nav-links > li").each((_, elem) => {
+                $(elem).css("opacity", "0")
+            })
+            open = false
+        } else if (!open) {
+            $(".nav-mobile").addClass("open")
+            const tl2 = gsap.timeline({ defaults: { ease: "power1.out" } })
+            tl2.fromTo('.nav-links > li', { opacity: 0, x: '-25%' }, { x: '0%', opacity: 1, duration: 0.5, stagger: 0.15, delay: 0.2 })
+            open = true
+        }
+
+
     })
 })
